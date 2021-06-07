692a94b6f5a8d9d4f62d96a9bf2b518ae97bd330
Calvin Roth <rothx195@umn.edu>
2021-06-07 13:01:06 -0500

    Update pie.js

--- js/pie.js
+++ js/pie.js
@@ -43,6 +43,7 @@
             text.innerHTML = cData.title;
             text.setAttribute("y", 11);
             text.setAttribute("x", 20);
+            text.setAttribute("class", "legitem"
 
             rect.setAttribute("fill", cData.color);
 
